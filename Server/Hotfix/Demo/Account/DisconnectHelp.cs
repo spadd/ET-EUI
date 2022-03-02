5498c7e721238b33901f7eeb00aab684014348b9
chen <766114552@qq.com>
2022-03-02 21:32:18 +0800

    更新到30 集，还有 seessionplayt destory 下线没有写

--- Server/Hotfix/Demo/Account/DisconnectHelp.cs
+++ Server/Hotfix/Demo/Account/DisconnectHelp.cs
@@ -47,8 +47,13 @@ namespace ET
                             break;
                         case PlayerState.Game:
                             // 通知游戏逻辑下线Unit角色逻辑，并将数据存入数据库
-                            MessageHelper.CallLocationActor(player.UnitId, new G2M_RequestEnterGameState());
-                            // 通知登录中心服 移除
+                            M2G_RequestExitGame m2GRequestEnterGameState = (M2G_RequestExitGame)await MessageHelper.CallLocationActor(player.UnitId, new G2M_RequestExitGame());
+                            // 通知 移除账号角色登录信息
+                            long LoginCenterConfigSceneId = StartSceneConfigCategory.Instance.LoginCenterConfig.InstanceId;
+                            L2G_RemoveLoginRecord l2GRemoveLoginRecord = (L2G_RemoveLoginRecord)await MessageHelper.CallActor(LoginCenterConfigSceneId,new G2L_RemoveLoginRecord()
+                            {
+                                AccountId = player.Account,ServerId = player.DomainZone()
+                            });
                             break;
                     }
                 }
--- Server/Hotfix/Demo/SessionPlayerComponentSystem.cs
+++ Server/Hotfix/Demo/SessionPlayerComponentSystem.cs
@@ -11,6 +11,9 @@ namespace ET
 				// 发送断线消息
 				ActorLocationSenderComponent.Instance.Send(self.PlayerId, new G2M_SessionDisconnect());
 				self.Domain.GetComponent<PlayerComponent>()?.Remove(self.AccountId);
+				
+				// todo 判断 主动断开（不执行kickplay） 还是 2次登录（执行KickPlay）
+				// DisconnectHelp.KickPlayer();
 			}
 		}
 
